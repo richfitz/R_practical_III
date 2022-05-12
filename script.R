5a3e6818b8f1feca83c56b26d2acd9ecb003fbd9
Nieves Derqui <n.derqui-fernandez@imperial.ac.uk>
2022-05-12 10:50:20 +0100

    Create project

---
+++ script.R
@@ -0,0 +1,60 @@
+
+    # MRes R Workshop III #
+
+
+
+# R NOTEBOOK --------------------------------------------------------------
+
+# R markdown
+
+# Open R Notebook
+
+# Normal text is just comments
+# This is how to insert the code:
+
+# ```{r}
+
+# Knit to show the "result"
+# Always run top-to-bottom
+# You can also produce a document to share
+
+
+
+# INTRODUCTION TO GIT -----------------------------------------------------
+
+# Why version control? : Metadata + History
+
+# WHO made changes
+# WHEN changes were made
+# WHAT content of the changes
+# WHY changes were made
+
+# BACK - UP
+
+# It is not a back-up
+# Back up your git repo
+# Do not use with OneDrive
+
+# 
+
+# repo
+# commit
+
+# Commands
+
+# pwd: print working directory
+# cd: change directory
+# ls: list
+# cp: copy
+# rm: remove
+
+# GIT Commands
+
+# git init
+# git status
+# git add
+# git commit
+# git log
+# git diff
+
+
